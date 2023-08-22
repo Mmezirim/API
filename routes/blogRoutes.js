bd61253d6eae92409de9acf07ac9c49aed26b13a
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2023-08-22 17:00:52 +0100

    Update blogRoutes.js

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -24,6 +24,7 @@ route.post('/api/blogs', (req, res) =>{
     })
     .catch((error) => {
       res.status(500).json({ message: 'Error saving data' });
+    });
 });
 
 route.get('/blogs', (req, res) =>{
