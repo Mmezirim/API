d5e02c094d79a53e5b801802c6b6779608a74652
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2023-08-22 21:22:04 +0100

    Update blogModel.js

--- models/blogModel.js
+++ models/blogModel.js
@@ -2,44 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const devSchema = new Schema({
-    title: {
-        type: String,
-        required: false
-    },
-    snippet:{
-        type: String,
-        required: false
-    },
-    body: mongoose.Schema.Types.Mixed,
-    image: {
-        type: String,
-        required: false
-    },
-    hashtags: {
-        type: String,
-        required: false
-    },
-    tags: {
-        type: [String],
-        required: false
-    },
-    author: {
-        type: String,
-        required: false
-    },readMins: {
-        type: String,
-        required: false
-    },categories: {
-        type: String,
-        required: false
-    },
-
-    comments:[
-        {
-            commenter: String,
-            text: String
-        }
-    ],
+  title: String,
+  snippet: String,
+  body: mongoose.Schema.Types.Mixed,
+  image: String,
+  hashtags: String,
+  tags: [String],
+  author: String,
+  readMins: String,
+  categories: String,
+  comments: [{ commenter: String, text: String }],
+});
     createdAt: {
         type: Date,
         default: Date.now
