81e732356b4762bc1647f4b7c080924834d28643
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2023-09-02 09:08:54 +0100

    Update DashAuth.js

--- routes/DashAuth.js
+++ routes/DashAuth.js
@@ -14,6 +14,7 @@ route.post('/register', async(req, res)=>{
             email: req.body.email,
             linkedInId: req.body.linkedInId,
             twitterId: req.body.twitterId,
+            access: req.body.access,
             password: passFilter,
         });
         const user = await newUser.save();
