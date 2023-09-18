aca84015cbe4d952b055cfbcafbc960c5ac647ac
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2023-09-18 23:48:12 +0100

    Update newsletters.js

--- models/newsletters.js
+++ models/newsletters.js
@@ -3,9 +3,7 @@ const Schema = mongoose.Schema;
 
 const newsLetter = new Schema({
     email: String,
-    name: String,
-    videoLink: String,
-    title: String,
+    fullname: String,
 },{timestamps: true});
 
 const Mail = mongoose.model('Mail', newsLetter);
