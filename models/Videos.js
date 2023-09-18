cbdc5c013a719f405adc2adef44a9181b6ff8cf1
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2023-09-18 23:52:36 +0100

    Update Videos.js

--- models/Videos.js
+++ models/Videos.js
@@ -1,3 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const videoUpload = new Schema({
     videoLink: String,
     title: String,
+},{timestamps: true});
+
+const VideoLinks = mongoose.model('VideoLinks', videoUpload);
+module.exports = VideoLinks;
