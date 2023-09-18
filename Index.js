2c87715867e8a0f633ca7fc1ca587609159ae6b5
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2023-09-18 23:40:51 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -17,8 +17,8 @@ const mongoURI = 'mongodb+srv://Kevin:Chibuoyim@cluster0.qyty0yo.mongodb.net/Kev
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connect;
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(BlogRoutes);
